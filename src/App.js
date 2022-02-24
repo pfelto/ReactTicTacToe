45394b2edd797b5fe87422f1e7d4d7de59139390
pfelto <paul.r.felton@gmail.com>
2022-02-24 11:37:59 -0500

    Playing around with using bootstrap purely for layout and styling as I am learning that in fCC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <h1 className="text-primary text-center">Player X, it's your turn!</h1>
+      <div className="row justify-content-center">
+        <div class="col-8" style={{ border: "2px solid black" }}>
+          <div className="row">
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+          </div>
+          <div className="row" style={{ border: "2px solid red" }}>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-primary btn-block">
+                <i className="fa fa-circle"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
